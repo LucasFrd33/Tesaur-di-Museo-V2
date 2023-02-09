a90dd6e823c1bd93a4633101cc291c45c8857e95
Maxenceclr <maxence.cuillierier@mmibordeaux.com>
2023-02-09 17:46:14 +0100

    first push

--- src/components/Motion1.js
+++ src/components/Motion1.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {useState} from 'react';
 import Lottie from 'react-lottie';
-import v1 from "../V1";
+import v1 from "../V1.json";
+
 
 function Motion1({handleStep,textButton}) {
 
