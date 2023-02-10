d70d08e842133a7ee6936b64930b0fd60941ead4
Mikail Dumlu <mikaildumlu@MacBook-Air-de-Mikail.local>
2023-02-10 11:56:37 +0100

    ajout de lap age quizz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -21,14 +21,14 @@ function Quiz({text, textButton, handleStep, title, image}) {
     // ];
 
   return (
-    <div className="container">
-      
+    <>
+    <div className="container-quiz">
       {title}
       <p>{text}</p>
       <button onClick={handleStep} >{textButton}</button>
-      {image}
     </div>
-    
+      {image}
+    </>
   );
 }
 
