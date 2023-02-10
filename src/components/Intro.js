780d93208623cf00bf8630a97bafd12f98e649ec
Maxenceclr <maxence.cuillierier@mmibordeaux.com>
2023-02-10 10:05:07 +0100

    no margin

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,13 +3,15 @@
 function Intro({text, textButton, image, handleStep, title}) {
 
   return (
+  <>
     <div className="container">
       {title}
-      <img className="img-bg-1" src="./img/image1.webp" alt="tt" />
       <div className="actions">
         <button className="button" onClick={handleStep} >{textButton}</button>
       </div>
     </div>
+      <img className="img-bg-1" src="./img/image1.webp" alt="tt" />
+</>
   );
 }
 
