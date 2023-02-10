bac184b32470ebbc6f5ac79968de5b08261191e7
LucasFrd33 <lucas.froidefond@mmibordeaux.com>
2023-02-10 10:20:05 +0100

    new version

--- src/App.js
+++ src/App.js
@@ -15,14 +15,13 @@ import Resume2 from './components/Resume2';
 import Answer3 from './components/Answer3';
 import Award3 from './components/Award3';
 import Finish from './components/Finish';
+import Transition from './components/Transition';
 
 
 function App(){
     const [stepSetup, setStepSetup] = useState(0);
 
     const stepPage = [
-
-
       {
         step:
         <Intro
@@ -32,7 +31,6 @@ function App(){
         title={<h1 className='titre'>Tesaur</h1>}
         />
       },
-      
       {
         step:
         <Motion1 
@@ -48,7 +46,6 @@ function App(){
         text={<p className='text-quiz'>Rendez-vous dans la salle : Les années 1910-1940 : du cubisme au retour à l’ordre.</p>}
         textButton="Jouer"
         handleStep={()=>handleStep(3)}
-        image={<img className="img-quiz-1" src="./img/group-122.webp" alt="tt" />}
         />
       },
       {
@@ -84,7 +81,18 @@ function App(){
         />
       },
       // Rajouter ici la page 11 du Figma
-
+      {
+        step:
+        <Transition
+        title={<h1>Tu as un petit message 
+          d’Albert Marquet :</h1>}
+        text={<p>Je te complimente d’avoir trouver le bon numéro !<br/>
+        Tu vas bientôt faire la connaissance d’un personnage que j’ai peint dans une de mes autres œuvres ! <br/>
+        Il est venu spécialement pour toi et va t’aider dans ta quête !<br/>J’étais heureux de rencontrer !</p>}
+        handleStep={()=>handleStep(8)}
+        textButton="Suivant"
+        />
+      },
 
       // Deuxieme jeu
       {
@@ -93,21 +101,21 @@ function App(){
           il va falloir lever les yeux du téléphone.</h1>}
         text={<p>Et être attentif à l’environnement autour de toi !</p>}
         textButton="Jouer"
+        handleStep={()=>handleStep(9)}
         image={<img className="img-quiz-1" src="./img/group-122.webp" alt="tt" />}
-        handleStep={()=>handleStep(7)}
         />
       },
       {
         step:
         <Motion3
-        handleStep={()=>handleStep(9)}
+        handleStep={()=>handleStep(10)}
         textButton="Suivant"
         />
       },
       {
         step:
         <Resume 
-        handleStep={()=>handleStep(10)}
+        handleStep={()=>handleStep(11)}
         textButton="Suivant"
         title={<h1>Résumé du 2ieme jeu </h1>}
         text={<p>Rend toi devant cette œuvre, Analyse bien le nom de ce tableau.<br/>
@@ -120,7 +128,7 @@ function App(){
       {
         step:
         <Answer2
-        handleStep={()=>handleStep(11)}
+        handleStep={()=>handleStep(12)}
         textButton="Suivant"
         />,
       },
@@ -128,31 +136,41 @@ function App(){
       {
         step:
         <Award2
-        handleStep={()=>handleStep(12)}
+        handleStep={()=>handleStep(13)}
         textButton="Suivant"
         />,
       },
-      // Rajouter  ici la page 19
+      {
+        step:
+        <Transition
+        title={<h1>L’homme au chapeau melon a un message pour toi : </h1>}
+        text={<p>Bravo, tu touches bientôt au but ! <br/>
+       Je te confie à la femme allongée de Joseph Rivière.<br/>
+       Elle te guidera pour découvrir le dernier numéro du cadenas. <br/>A bientôt !</p>}
+        handleStep={()=>handleStep(14)}
+        textButton="Suivant"
+        />
+      },
       {
         step: <Quiz 
         title={<h1>Pour ce 3ème jeu, 
           il va falloir lever les yeux du téléphone.</h1>}
         text={<p>Et être attentif à l’environnement autour de toi !</p>}
         textButton="Jouer"
-        handleStep={()=>handleStep(13)}
+        handleStep={()=>handleStep(15)}
         />
       },
       {
         step:
         <Motion4
-        handleStep={()=>handleStep(14)}
+        handleStep={()=>handleStep(16)}
         textButton="Suivant"
         />
       },
       {
         step:
         <Resume2
-        handleStep={()=>handleStep(15)}
+        handleStep={()=>handleStep(17)}
         textButton="Suivant"
         title={<h1>Résumé du 3ème jeu :</h1>}
         text={<p>Observe bien le tableau de Willem Van Hasselt et sa reproduction ci-dessous ! 
@@ -163,22 +181,22 @@ function App(){
       {
         step:
         <Answer3
-        handleStep={()=>handleStep(16)}
+        handleStep={()=>handleStep(18)}
         textButton="Suivant"
         />,
       },
       {
         step:
         <Award3
-        handleStep={()=>handleStep(17)}
+        handleStep={()=>handleStep(19)}
         textButton="Suivant"
         />,
       },
       {
         step:
         <Finish
-        handleStep={()=>handleStep()}
-        textButton="Suivant"
+        handleStep={()=>handleStep(1)}
+        textButton="BRAVO"
         />,
       },
     ]
