8cae631e460db6df159816e4e70c5b0ff79e2361
LucasFrd33 <lucas.froidefond@mmibordeaux.com>
2023-02-12 13:41:47 +0100

    finish2

--- src/components/Motion2.js
+++ src/components/Motion2.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState} from 'react';
 import Lottie from 'react-lottie';
-import v1 from "../V1-tbl2.json";
+import v1 from "../V1-tbl1.json";
 import AudioSimple from './Audio';
 
 function Motion2({handleStep,textButton, image}) {
